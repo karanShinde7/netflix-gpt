801cad79f6ad55883b66f9816fa46876fa931f41
karanShinde7 <karan.shinde27@gmail.com>
2024-07-23 15:25:37 +0530

    Redux setup for movie list

--- src/redux/store/appStore.js
+++ src/redux/store/appStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slice/userSlice";
+import movieReducer from "../slice/movieSlice";
 
 const appStore = configureStore({
-    reducer: {
-        user: userReducer
-    }
+  reducer: {
+    user: userReducer,
+    movie: movieReducer,
+  },
 });
 
 export default appStore;
