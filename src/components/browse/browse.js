3f127b9cf019a1cf1576cabe83d6dac60e4f67c9
karanShinde7 <karan.shinde27@gmail.com>
2024-07-23 15:31:05 +0530

    Custom hook with API call

--- src/components/browse/browse.js
+++ src/components/browse/browse.js
@@ -1,22 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "../header/header";
-import { GET_NOW_PLAYING_MOVIES_URL } from "../../utils/urls";
-import { GET_API_OPTION } from "../../utils/apiConstants";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../../redux/slice/movieSlice";
+import useNowPlayingMovies from "../../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
-
-  const getNowPlayingMovies = async () => {
-    const moviesData = await fetch(GET_NOW_PLAYING_MOVIES_URL, GET_API_OPTION);
-    const jsonResponse = await moviesData.json();
-    dispatch(addNowPlayingMovies(jsonResponse));
-  };
+  // API call with custom hook
+  useNowPlayingMovies();
 
   return (
     <div>
---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { GET_NOW_PLAYING_MOVIES_URL } from "../utils/urls";
+import { GET_API_OPTION } from "../utils/apiConstants";
+import { addNowPlayingMovies } from "../redux/slice/movieSlice";
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
+  const getNowPlayingMovies = async () => {
+    const moviesData = await fetch(GET_NOW_PLAYING_MOVIES_URL, GET_API_OPTION);
+    const jsonResponse = await moviesData.json();
+    dispatch(addNowPlayingMovies(jsonResponse));
+  };
+};
+
+export default useNowPlayingMovies;
