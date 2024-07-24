18411c091ff3fae9407842c2eadd8e3010d105f5
karanShinde7 <karan.shinde27@gmail.com>
2024-07-24 09:48:48 +0530

    Multi language support

--- src/redux/store/appStore.js
+++ src/redux/store/appStore.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slice/userSlice";
 import movieReducer from "../slice/movieSlice";
+import gptSearchReducer from "../slice/gptSearchSlice";
+import configSliceReducer from "../slice/configSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
     movies: movieReducer,
+    gptSearch: gptSearchReducer,
+    config: configSliceReducer
   },
 });
 
